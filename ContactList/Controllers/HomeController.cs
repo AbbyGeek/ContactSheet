2bb8aa1e5572c1bb5dfa57070beac9fada5c432f
Abby West <abbyrosewest@gmail.com>
2020-02-05 13:15:23 -0500

    initial commit

---
+++ ContactList/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContactList.Models;
+
+namespace ContactList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var contacts = GetContacts();
+            return View(contacts);
+        }
+
+        private IEnumerable<Contact> GetContacts()
+        {
+            return new List<Contact>
+            {
+                new Contact() {id=1,firstName = "John", lastName="Smith", company="Working Company", address="123 home lane", email="ab@123.com", primaryPhone=1234432343, secondaryPhone=5555555555},
+                new Contact() {id=2, firstName = "John", lastName="Smith II", company="Working Company", address="123 home lane", email="ab@123.com", primaryPhone=1234432343, secondaryPhone=5555555555},
+                new Contact() {id=3, firstName = "John", lastName="Smith III", company="Working Company", address="123 home lane", email="ab@123.com", primaryPhone=1234432343, secondaryPhone=5555555555}
+            };
+        }
+
+
+        public ActionResult AddContact()
+        {
+            ViewBag.Message = "Add Contact";
+            return View();
+        }
+
+        public ActionResult ViewContact(int id)
+        {
+            var contact = GetContacts().SingleOrDefault(c => c.id == id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+    }
+}
---
+++ ContactList/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactList.Models
+{
+    public class Contact
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string company { get; set; }
+        public string email { get; set; }
+        public long primaryPhone { get; set; }
+        public long secondaryPhone { get; set; }
+        public string address { get; set; }
+
+    }
+}
